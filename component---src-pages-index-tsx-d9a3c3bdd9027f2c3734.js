747a8928c16cd7aeb1799faab9aaf4b9f1fd3c1b
manhanh1002 <manhanh0210@gmail.com>
2026-01-18 15:46:13 +0700

    Static build of CDP landing page

---
+++ component---src-pages-index-tsx-d9a3c3bdd9027f2c3734.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkesoft_gatsby=self.webpackChunkesoft_gatsby||[]).push([[691],{7200:function(e,t,a){a.r(t),a.d(t,{Head:function(){return u}});var n=a(7294),s=a(1816);t.default=()=>n.createElement(s.default,null);const u=s.Head}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-d9a3c3bdd9027f2c3734.js.map
